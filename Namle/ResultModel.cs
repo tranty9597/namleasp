807137cb0cf2d5601331d4c68009160714d83559
Tran <tranty9597@gmail.com>
2019-04-25 21:25:55 +0700

    ok

---
+++ Namle/ResultModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Namle
+{
+    public class ResultModel
+    {
+        bool success;
+
+        public AbcModel value;
+
+        public ResultModel(bool success, AbcModel value)
+        {
+            this.success = success;
+            this.value = value;
+        }
+    }
+}
